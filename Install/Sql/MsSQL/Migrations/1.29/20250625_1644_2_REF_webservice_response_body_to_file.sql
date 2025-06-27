798356cbc0469937b78d6c526d31301b8416b766
hail-cookies <81558994+hail-cookies@users.noreply.github.com>
2025-06-27 12:19:03 +0200

    DEV WebserviceRequest mirgations

---
+++ Install/Sql/MsSQL/Migrations/1.29/20250625_1644_2_REF_webservice_response_body_to_file.sql
@@ -0,0 +1,34 @@
+-- UP
+
+ALTER TABLE etl_webservice_response ADD body_file NVARCHAR(200);
+UPDATE etl_webservice_response SET body_file = CONCAT('responses/', LOWER(CONVERT(VARCHAR(MAX),oid,1)), '/body.json');
+
+/* 
+Save contents of the `response_body` as files to the paths found in the column `body_file`.
+
+@plugin.WriteSqlRowsToFiles(
+	'SELECT response_body, body_file FROM etl_webservice_response WHERE response_body IS NOT NULL;',
+	'axenox.ETL.webservice_request_storage',
+	'body_file',
+	'response_body'
+);
+*/
+
+ALTER TABLE etl_webservice_response DROP COLUMN response_body;
+
+-- DOWN
+
+ALTER TABLE etl_webservice_response ADD response_body NVARCHAR(MAX);
+
+/*
+@plugin.WriteFilesToSqlRows(
+    'SELECT body_file, LOWER(CONVERT(VARCHAR(MAX),oid,1)) as oid FROM etl_webservice_response WHERE body_file IS NOT NULL;',
+    'UPDATE etl_webservice_response SET response_body = [#content#] WHERE LOWER(CONVERT(VARCHAR(MAX),oid,1))) = [#key#];',
+    'body_file',
+	'oid',
+	'axenox.ETL.webservice_request_storage'
+);
+*/
+
+ALTER TABLE etl_webservice_response DROP COLUMN body_file;
+
---
+++ Install/Sql/MySQL/Migrations/1.29/20250625_1644_2_REF_webservice_request_body_to_file.sql
@@ -0,0 +1,34 @@
+-- UP
+
+ALTER TABLE `etl_webservice_request` ADD `body_file` NVARCHAR(200);
+UPDATE `etl_webservice_request` SET `body_file` = CONCAT("requests/", CONCAT('0x', LOWER(HEX(`oid`))), "/body.json");
+
+/* 
+Save contents of the `http_body` as files to the paths found in the column `body_file`.
+
+@plugin.WriteSqlRowsToFiles(
+	'SELECT http_body, body_file FROM etl_webservice_request WHERE http_body IS NOT NULL;',
+	'axenox.ETL.webservice_request_storage',
+	'body_file',
+	'http_body'
+);
+*/
+
+ALTER TABLE `etl_webservice_request` DROP COLUMN `http_body`;
+
+-- DOWN
+
+ALTER TABLE `etl_webservice_request` ADD `http_body` LONGTEXT;
+
+/*
+@plugin.WriteFilesToSqlRows(
+    'SELECT body_file, CONCAT('0x', LOWER(HEX(`oid`))) as oid FROM etl_webservice_request WHERE body_file IS NOT NULL;',
+    'UPDATE etl_webservice_request SET http_body = [#content#] WHERE CONCAT('0x', LOWER(HEX(`oid`))) = [#key#];',
+    'body_file',
+	'oid',
+	'axenox.ETL.webservice_request_storage'
+);
+*/
+
+ALTER TABLE `etl_webservice_request` DROP COLUMN `body_file`;
+
