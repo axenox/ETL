cc23ba3d88938cbb938d46f691723f02f395abbf
hail-cookies <81558994+hail-cookies@users.noreply.github.com>
2025-11-21 18:15:42 +0100

    FIX OpenAPI3 improved example generation (#171)
    * FIX OpenAPI3MetaModelSchemabuilder loadExamples now loads specified group
    
    * DEV OpenAPI3 added config option to hide examples
    
    * DEV SwaggerUiMiddleWare adde config option toggle "Try it out"
    
    * DEV OpenAPI3 improved example generation

--- Common/OpenAPI/OpenAPI3MetaModelSchemaBuilder.php
+++ Common/OpenAPI/OpenAPI3MetaModelSchemaBuilder.php
@@ -1,21 +1,15 @@
 <?php
 namespace axenox\ETL\Common\OpenAPI;
 
-use axenox\ETL\Common\SqlColumnMapping;
 use exface\Core\Behaviors\TimeStampingBehavior;
-use exface\Core\CommonLogic\DataSheets\DataColumn;
-use exface\Core\CommonLogic\Model\Attribute;
 use exface\Core\DataTypes\ComparatorDataType;
 use exface\Core\DataTypes\HexadecimalNumberDataType;
 use exface\Core\DataTypes\StringDataType;
 use exface\Core\DataTypes\TimeDataType;
 use exface\Core\Exceptions\Model\MetaModelLoadingFailedError;
-use exface\Core\Factories\AttributeListFactory;
 use exface\Core\Factories\DataSheetFactory;
-use exface\Core\Factories\DataSorterFactory;
 use exface\Core\Interfaces\DataSheets\DataSheetInterface;
 use exface\Core\Interfaces\DataTypes\DataTypeInterface;
-use exface\Core\Interfaces\Model\MetaAttributeInterface;
 use exface\Core\Interfaces\Model\MetaObjectInterface;
 use exface\Core\DataTypes\IntegerDataType;
 use exface\Core\DataTypes\NumberDataType;
@@ -26,8 +20,6 @@ use exface\Core\DataTypes\DateTimeDataType;
 use exface\Core\DataTypes\DateDataType;
 use exface\Core\DataTypes\BinaryDataType;
 use exface\Core\Exceptions\InvalidArgumentException;
-use PhpOffice\PhpSpreadsheet\Calculation\DateTimeExcel\TimeValue;
-use function Sodium\add;
 
 /**
  * A builder that creates a schema from a MetaObjectInterface.
@@ -69,7 +61,7 @@ class OpenAPI3MetaModelSchemaBuilder
      * Create a json schema for the given meta object. Uses all DataType classes supported by JsonSchema.
      *
      * @param MetaObjectInterface $metaObject
-     * @param array $attributeAliasesToAdd
+     * @return array
      */
     public function transformIntoJsonSchema(MetaObjectInterface $metaObject): array
     {
@@ -90,7 +82,13 @@ class OpenAPI3MetaModelSchemaBuilder
 
         if ($this->loadExamples) {
             try {
-                $richestRow = self::loadExamples($metaObject);
+                $richestRow = self::getExampleRow(
+                    $metaObject, 
+                    $metaObject->getAliasWithNamespace(), 
+                    $metaObject->getAttributes()->getAll(),
+                    true,
+                    -1
+                );
             } catch (\Throwable $exception) {
                 $metaObject->getWorkbench()->getLogger()->logException(new MetaModelLoadingFailedError(
                     'Could not load example data for "' . $metaObject->getAliasWithNamespace() . '".',
@@ -149,70 +147,91 @@ class OpenAPI3MetaModelSchemaBuilder
     }
 
     /**
-     * Loads example values for all attributes in a given MetaObject.
+     * Returns an example row with all the attributes provided.
      *
      * NOTE: Results will be stored in cache and are kept for up to one hour.
      * If you wish to refresh your examples sooner than that, clear the cache.
      *
      * @param MetaObjectInterface $metaObject
-     * @param bool                $hideRelations
+     * @param string              $cacheKey
+     * @param array               $attributes
+     * @param bool                $stitchData
+     * If TRUE, the returned row actually exists in the source data. If FALSE the resulting row will be stitched
+     * together from multiple rows to maximize the number of columns containing meaningful data.
+     * @param int                 $count
+     * The number of rows to be checked for example data. Rows will be loaded from the bottom of the source data.
+     * Use -1 to load all available source data.
      * @return array|null
      */
-    public static function loadExamples(MetaObjectInterface $metaObject, bool $hideRelations = true) : ?array 
+    public static function getExampleRow(
+        MetaObjectInterface $metaObject,
+        string              $cacheKey,
+        array               $attributes,
+        bool                $stitchData,
+        int                 $count
+    ) : ?array
     {
-        $fromCache = self::loadExamplesFromCache($metaObject, $hideRelations);
+        $fromCache = self::loadExamplesFromCache($metaObject, $cacheKey);
         if($fromCache !== null) {
             return $fromCache;
         }
-        
-        $ds = DataSheetFactory::createFromObject($metaObject);
+
         $columns = [];
-        foreach ($metaObject->getAttributes()->getAll() as $attr) {
+        $labelColumns = [];
+        foreach ($attributes as $attr) {
+            $shortAlias = $attr->getAlias();
             if($attr->isRelation()) {
-                $rightObj = $attr->getRelation()->getRightObject();
-                $alias = $attr->getAliasWithRelationPath() . ($rightObj->hasLabelAttribute() ? '__LABEL' : '');
+                $columns[$shortAlias] = $attr->getAliasWithRelationPath();
+                if($attr->getRelation()->getRightObject()->hasLabelAttribute()) {
+                    $labelAlias = $shortAlias . '__LABEL';
+                    $labelColumns[$labelAlias] = $labelAlias;
+                }
             } else {
-                $alias = $attr->getAlias();
+                $columns[$shortAlias] = $shortAlias;
             }
-            
-            $columns[$attr->getAlias()] = $alias;
+
         }
 
-        $ds->getColumns()->addMultiple($columns);
+        $ds = DataSheetFactory::createFromObject($metaObject);
 
         if (($tsBehavior = $metaObject->getBehaviors()->getByPrototypeClass(TimeStampingBehavior::class)->getFirst()) != null){
             $orderingAttribute = $tsBehavior->getUpdatedOnAttribute();
             $ds->getSorters()->addFromString($orderingAttribute->getAlias(), 'DESC');
         }
 
-        if ($ds->hasUidColumn()){
-            $rowWithRelations = self::getRowWithTheLeastNullValues($ds);
-            
-            if($rowWithRelations === null) {
+        if(!$ds->hasUidColumn()) {
+            if($metaObject->hasUidAttribute()) {
+                $ds->getColumns()->addFromAttribute($metaObject->getUidAttribute());
+            } else {
                 return null;
             }
-            
-            $rowHideRelations = [];
-            foreach ($columns as $shortAlias => $fullAlias) {
-                $rowHideRelations[$shortAlias] = $rowWithRelations[$fullAlias] ?? $rowWithRelations[$shortAlias];
-            }
-            
-            self::storeExamplesInCache($metaObject, $rowHideRelations, $rowWithRelations);
-            
-            return $hideRelations ? $rowHideRelations : $rowWithRelations;
         }
-        
-        return null;
+
+        $rowWithRelations = $stitchData ?
+            self::getStitchedRow($ds, $columns, $labelColumns) : 
+            self::getRowWithTheLeastNullValues($ds, $columns, $labelColumns, $count);
+
+        if($rowWithRelations === null) {
+            return null;
+        }
+
+        $rowHideRelations = [];
+        foreach ($columns as $shortAlias => $fullAlias) {
+            $rowHideRelations[$shortAlias] = $rowWithRelations[$fullAlias] ?? $rowWithRelations[$shortAlias];
+        }
+
+        self::storeExamplesInCache($metaObject, $cacheKey, $rowHideRelations);
+        return $rowHideRelations;
     }
 
     /**
      * @param MetaObjectInterface $metaObject
-     * @param bool                $hideRelations
+     * @param string              $key
      * @return array|null
      */
-    private static function loadExamplesFromCache(MetaObjectInterface $metaObject, bool $hideRelations) : ?array
+    private static function loadExamplesFromCache(MetaObjectInterface $metaObject, string $key) : ?array
     {
-        $cacheKey = self::getCacheKey($metaObject);
+        $cacheKey = self::getCacheKey($key);
         $cache = $metaObject->getWorkbench()->getCache();
         
         try {
@@ -229,28 +248,27 @@ class OpenAPI3MetaModelSchemaBuilder
             return null;
         }
         
-        return $hideRelations ? $fromCache['rowHideRelations'] : $fromCache['rowWithRelations'];
+        return $fromCache['row'];
     }
 
     /**
      * @param MetaObjectInterface $metaObject
-     * @param array               $rowHideRelations
-     * @param array               $rowWithRelations
+     * @param string              $key
+     * @param array               $row
      * @return void
      */
     private static function storeExamplesInCache(
-        MetaObjectInterface $metaObject, 
-        array               $rowHideRelations, 
-        array $rowWithRelations
+        MetaObjectInterface $metaObject,
+        string              $key,
+        array               $row 
     ) : void
     {
         try {
             $metaObject->getWorkbench()->getCache()->set(
-                self::getCacheKey($metaObject),
+                self::getCacheKey($key),
                 [
                     'expires' => time() + 3600,
-                    'rowHideRelations' => $rowHideRelations,
-                    'rowWithRelations' => $rowWithRelations
+                    'row' => $row,
                 ]
             );
         } catch (\Psr\SimpleCache\InvalidArgumentException $e) {
@@ -259,42 +277,93 @@ class OpenAPI3MetaModelSchemaBuilder
     }
 
     /**
-     * @param MetaObjectInterface $metaObject
+     * @param string $key
      * @return string
      */
-    private static function getCacheKey(MetaObjectInterface $metaObject) : string
+    private static function getCacheKey(string $key) : string
     {
-        return 'SchemaExamples__' . str_replace('.', '_', $metaObject->getAliasWithNamespace());
+        return 'SchemaExamples__' . $key;
     }
 
-    /**
-     * @param DataSheetInterface $ds
-     * @return mixed|null
-     */
-    private static function getRowWithTheLeastNullValues(DataSheetInterface $ds) : ?array
+    private static function getStitchedRow(
+        DataSheetInterface $dataSheet, 
+        array $columns,
+        array $labelColumns
+    ) : array
     {
-        $ds->dataRead(1);
-        $row = $ds->getRow();
+        $result = [];
         
-        if($row === null) {
-            return null;
-        }
-        
-        foreach ($row as $col=>$value) {
-            if ($value !== null) {
-                continue;
-            }
-            
-            $ds->getFilters()->addConditionFromString(
-                $col,
+        foreach ($columns as $column) {
+            $columnSheet = $dataSheet->copy();
+            $columnSheet->getColumns()->addFromExpression($column);
+            $columnSheet->getFilters()->addConditionFromString(
+                $column,
                 EXF_LOGICAL_NULL,
                 ComparatorDataType::IS_NOT
             );
             
-            return self::getRowWithTheLeastNullValues($ds) ?? $row;
+            $labelColumn = $column . '__LABEL';
+            if(key_exists($labelColumn, $labelColumns)) {
+                $columnSheet->getColumns()->addFromExpression($labelColumn);
+            }
+
+            try {
+                $columnSheet->dataRead(1);
+                $row = $columnSheet->getRow();
+                if(!empty($row)) {
+                    $result[$column] = $row[$labelColumn] ?? $row[$column];
+                }
+            } catch (\Throwable $e) {
+
+            }
         }
         
-        return $row;
+        return $result;
+    }
+
+    /**
+     * @param DataSheetInterface $dataSheet
+     * @param array              $columns
+     * @param array              $labelColumns
+     * @param int                $count
+     * @return mixed|null
+     */
+    private static function getRowWithTheLeastNullValues(
+        DataSheetInterface $dataSheet, 
+        array $columns,
+        array $labelColumns,
+        int $count
+    ) : ?array
+    {
+        $dataSheet->getColumns()->addMultiple($columns);
+        $dataSheet->getColumns()->addMultiple($labelColumns);
+        try {
+            $dataSheet->dataRead($count > 0 ? $count : null);
+        } catch (\Throwable $e) {
+
+        }
+        
+        $bestRow = null;
+        $mostValues = 0;
+        
+        foreach ($dataSheet->getRows() as $row) {
+            $values = 0;
+            foreach ($columns as $column) {
+                $labelColumn = $column . '__LABEL';
+                $value = $row[$labelColumn] ?? $row[$column];
+
+                if ($value !== null) {
+                    $values++;
+                }
+            }
+            
+            if($values > $mostValues) {
+                $bestRow = $row;
+                $mostValues = $values;
+            }
+        }
+        
+        return $bestRow;
     }
 
     /**
